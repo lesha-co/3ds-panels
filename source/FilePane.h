067e4e485dddb90b3b75b6c75f0e9ab69d9965f8
malyavkin <a@malyavk.in>
2016-09-23 20:11:30 +0300

    add cursor movement

--- source/FilePane.h
+++ source/FilePane.h
@@ -5,50 +5,203 @@
 #ifndef MAIN_EXEC_FILEPANE_H
 #define MAIN_EXEC_FILEPANE_H
 
-
+#include <fstream>
 #include <3ds.h>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <sys/stat.h>
-
+#include <sstream>
+#define  UPDIR ".."
+template <class T>
+inline std::string to_string (const T& t) {
+    std::stringstream ss;
+    ss << t;
+    return ss.str();
+}
 using namespace std;
-
+typedef struct dirent dirent;
+const u8 FILENAME_WIDTH = 16;
 struct FileInfo{
     string name;
     string path;
+    bool special;
     struct stat stats;
 };
 
-class FilePane {
-public:
-    FilePane(PrintConsole printConsole) {
-        this->printConsole = printConsole;
+vector<FileInfo> list_files(string dir){
+    vector<FileInfo> v;
+    DIR *dp;
+    dirent *dirp;
+    string pref = "/";
+
+    dp = opendir( (pref +dir).c_str() );
+    if (dp == NULL)
+    {
+        cout << "Error(" << errno << ") opening " << dir << endl;
+        return v;
+
+    } else {
+        while ((dirp = readdir( dp )))
+        {
+            FileInfo fi;
+            fi.name = dirp->d_name;
+            fi.path = dir + "/" + dirp->d_name;
+
+            stat( fi.path.c_str(), &fi.stats );
+
+            v.push_back(fi);
+        }
+        closedir( dp );
     }
+    return v;
+}
 
+
+class FilePane {
+    // special case:
+    // index 0 represents UP-DIR (..)
+    // index X represents items[X-1]
+public:
+    FilePane(PrintConsole printConsole, string cwd) {
+        this->printConsole = printConsole;
+        this->startingIndex = 0;
+        this->selectedItem = 0;
+        this->active = false;
+        this->updir.name=UPDIR;
+        this->updir.special = true;
+        this->setCWD(cwd);
+        this->draw();
+    }
+    void setActive(bool active){
+        this->active = active;
+        draw();
+    }
     void setSelectedItemIndex(u32 item) {
-
+        this->selectedItem = item;
     }
 
     u32 getSelectedItemIndex() {
-        return 0;
+        return this->selectedItem;
     }
-
-    string getSelectedItem() {
-        return std::__cxx11::string();
+    FileInfo getItem(u32 index) {
+        if(index == 0){
+            return updir;
+        } else {
+            return this->items[index-1];
+        }
+    }
+    FileInfo getSelectedItem() {
+        return getItem(this->selectedItem);
     }
 
     string getCWD() {
-        return std::__cxx11::string();
+        return this->cwd;
     }
 
     void setCWD(string cwd) {
+        this->cwd = cwd;
+        this->items = list_files(this->cwd);
+    }
+
+    string getTypeIcon(FileInfo info) {
+
+        if(S_ISDIR(info.stats.st_mode)){
+            return "/";
+        }
+        if(info.special){
+            if(info.name == UPDIR){
+                return "/";
+            }
+        }
+        return "?";
 
     }
 
+    void draw(){
+        consoleSelect(&this->printConsole);
+
+        for (u32 i = 0; i < this->getDisplayHeight() ; ++i) {
+            cout << position(i,0);
+            u32 drawingItem = i + this->startingIndex;
+            cout << setColor(COLOR_WHITE, COLOR_BLUE) << "|";
+            if(drawingItem == this->selectedItem && this->active) {
+                cout << setColor(COLOR_BLACK, COLOR_CYAN);
+            }
+            string filename = "";
+            string type = " ";
+            if(drawingItem <= this->getMaxIndex()){
+                filename = this->getItem(drawingItem).name;
+                type = getTypeIcon(this->getItem(drawingItem));
+            }
+
+            cout << type << this->prepareFilename(filename) << "|" << "     ";
+            if(drawingItem == this->selectedItem && this->active) {
+                cout << setColor(COLOR_WHITE, COLOR_BLUE);
+            }
+            cout  << "|";
+        }
+    }
+    void moveUp(){
+        if(this->selectedItem > 0){
+            this->selectedItem--;
+            // если курсор совпадает с самым верхним отображающимся элементом
+            if (this->selectedItem == this->startingIndex){
+                // если после перемещения курсор не стал совпадать с самым первым элементом
+                if (this->selectedItem > 0){
+                    //двигаем окно вверх
+                    this->startingIndex = this->selectedItem-1;
+                }
+            }
+            draw();
+        }
+    }
+    void moveDown(){
+        if(this->selectedItem < this->getMaxIndex()){
+            this->selectedItem++;
+            // если курсор совпадает с самым нижним отображающимся элементом
+            if (this->selectedItem == this->getBottomIndex()){
+                // если после перемещения курсор не стал совпадать с самым последним элементом
+                if (this->selectedItem < this->getMaxIndex()){
+                    //двигаем окно вниз
+                    this->startingIndex++;
+                }
+            }
+            draw();
+        }
+    }
+    void enter(){
+
+    }
 private:
+    string prepareFilename(string s){
+        string s1 = s.substr(0, FILENAME_WIDTH);
+        while (s1.length() < FILENAME_WIDTH){
+            s1+=" ";
+        }
+        return s1;
+    }
+    u32 getDisplayHeight(){
+        int ht = this->printConsole.windowHeight;
+        if (ht < 0){
+            return 0;
+        }
+        return (u32)ht;
+    };
+    u32 getBottomIndex(){
+        return this->startingIndex + this->getDisplayHeight() - 1;
+    }
+    u32 getMaxIndex(){
+        // not doing -1 because we have extra item on top [..]
+        return this->items.size();
+    }
     PrintConsole printConsole;
     vector<FileInfo> items;
-    //static const string fmt = "|@bzcmp           | 353M|";
+    FileInfo updir;
+    u32 selectedItem;
+    u32 startingIndex;
+    string cwd;
+    bool active;
 };
 
 
--- source/main.cpp
+++ source/main.cpp
@@ -1,43 +1,16 @@
 #include <3ds.h>
 #include <cstdio>
 #include <iostream>
-
-#include <fstream>
-
 #include <dirent.h>
 #include "../3ds_string_utils/source/string_utils.h"
 #include "FilePane.h"
 
 using namespace std;
 
-vector<FileInfo> readdir(string dir){
-    vector<FileInfo> v;
-    DIR *dp;
-    struct dirent *dirp;
-    string pref = "/";
+void a(vector<int>* c){
+    c->push_back(1);
 
-    dp = opendir( (pref +dir).c_str() );
-    if (dp == NULL)
-    {
-        cout << "Error(" << errno << ") opening " << dir << endl;
-        return v;
-
-    } else {
-        while ((dirp = readdir( dp )))
-        {
-            FileInfo fi;
-            fi.name = dirp->d_name;
-            fi.path = dir + "/" + dirp->d_name;
-
-            stat( fi.path.c_str(), &fi.stats );
-
-            v.push_back(fi);
-        }
-        closedir( dp );
-    }
-    return v;
 }
-
 int main(int argc, char **argv)
 {
     gfxInitDefault();
@@ -45,14 +18,13 @@ int main(int argc, char **argv)
     /// run once begin
     ///
     consoleInit(GFX_TOP, NULL);
-    /*
-    PrintConsole menu, leftpan, rightpan, console, actions, context;
-
+    PrintConsole menu, leftpan, rightpan, console, actions, context, all;
     consoleInit(GFX_TOP, &leftpan);
     consoleInit(GFX_TOP, &rightpan);
     consoleInit(GFX_TOP, &console);
     consoleInit(GFX_TOP, &actions);
     consoleInit(GFX_TOP, &context);
+    consoleInit(GFX_TOP, &all);
 
                     //           x   y  w   h
     consoleSetWindow(&menu,      0,  0, 50, 1);
@@ -61,22 +33,12 @@ int main(int argc, char **argv)
     consoleSetWindow(&console,   0, 27, 50, 2);
     consoleSetWindow(&actions,   0, 29, 50, 1);
     consoleSetWindow(&context,   5, 1, 20, 20);
-    */
-    vector<FileInfo> f = readdir("");
-    vector<FileInfo> f1 = readdir("!a9lh");
-    printf("DCIM/100NIN03 %d\n", f.size());
-    for (FileInfo fi:f) {
-        printf("%s %s\n", S_ISDIR(fi.stats.st_mode)?"DIR":"FIL", fi.name.c_str());
-    }
-    printf("a9lh %d\n", f1.size());
 
-    for (FileInfo fi:f1) {
-        printf("%s %s\n", S_ISDIR(fi.stats.st_mode)?"DIR":"FIL", fi.name.c_str());
-    }
-
-
-
-
+    FilePane leftFilePane(leftpan, "");
+    FilePane rightFilePane(rightpan, "");
+    FilePane* active;
+    active = &leftFilePane;
+    leftFilePane.setActive(true);
 
 
     ///
@@ -86,11 +48,31 @@ int main(int argc, char **argv)
     while (aptMainLoop())
     {
         hidScanInput();
+        u32 kHeld = hidKeysHeld();
         u32 kDown = hidKeysDown();
         if (kDown & KEY_START) break; // break in order to return to hbmenu
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         /// run loop begin
         ///
+        if ((kHeld | kDown) & KEY_DOWN){
+            active->moveDown();
+        }
+        if ((kHeld | kDown) & KEY_UP){
+            active->moveUp();
+        }
+        if (kDown & KEY_A){
+            active->enter();
+        }
+        if (kDown & KEY_DLEFT){
+            active = &leftFilePane;
+            leftFilePane.setActive(true);
+            rightFilePane.setActive(false);
+        }
+        if (kDown & KEY_DRIGHT){
+            active = &rightFilePane;
+            rightFilePane.setActive(true);
+            leftFilePane.setActive(false);
+        }
 
         ///
         /// run loop end
