8ed0ab9a2545c51cf12a815b97df9601f230c004
malyavkin <a@malyavk.in>
2016-09-24 01:20:34 +0300

    i probably will need this to find more characters to use

--- source/common.h
+++ source/common.h
@@ -8,4 +8,18 @@
 #include <3ds.h>
 
 PrintConsole menu, leftpan, rightpan, console, actions, context, all, bottom;
+
+void drawASCII(){
+    consoleSelect(&bottom);
+    for (int i = 0; i < 256; ++i) {
+        if( i%16 == 0) {
+            printf("\n %3x |", i);
+        }
+        if(i == '\n' || i == '\r' || i == '\0' ){
+            printf("X ");
+        } else {
+            printf("%-2c",i);
+        }
+    }
+}
 #endif //MAIN_EXEC_COMMON_H
